5a5d10e3c7d02a4722bd8dbb1a6572dd8a4d17f8
Zuuu <zuuu112233@gmail.com>
2024-01-20 21:31:34 +0700

    ShareLocalV1.4

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 from flask_socketio import SocketIO
 from threading import Thread
 import os
 from werkzeug.utils import secure_filename
 
-
-UPLOAD_FOLDER = 'uploads'  # Folder untuk menyimpan file yang di-upload
-
+UPLOAD_FOLDER = 'uploads'
 app = Flask('')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 socketio = SocketIO(app)
 
 @app.route('/')
 def home():
-    files = os.listdir(UPLOAD_FOLDER)  # Dapatkan daftar file yang sudah di-upload
+    files = os.listdir(UPLOAD_FOLDER)
     return render_template('index.html', files=files)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file']  # Ambil file dari form
-        filename = secure_filename(file.filename)  # Amankan nama file
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  # Simpan file
-        return 'File uploaded successfully!'
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('upload_success'))
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)  # Kirim file yang di-request
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/upload-success')
+def upload_success():
+    return render_template('upload.html')
 
 def run():
     socketio.run(app, host='0.0.0.0', port=8080, debug=True, use_reloader=False)
